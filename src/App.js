88b9dae2c35544b225b1ef05afa4631e1877cd66
Majd Eddine BEN TAHAR <45672105+Majd-eddine-BEN-TAHAR@users.noreply.github.com>
2020-08-29 12:21:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1 className="bg-red-500 text-white">hello world</h1>
+      <h1 className="bg-red-500 text-white font-noto">hello world</h1>
     </div>
   );
 }
